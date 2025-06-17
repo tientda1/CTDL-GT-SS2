71996d81530385478db35cd3d894e960c7bc273c
tientda1 <110662513+tientda1@users.noreply.github.com>
2025-06-17 20:28:23 +0700

    Add files via upload

---
+++ Session 2-Ex1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int arr[100];
+    int max;
+
+    do {
+        printf("Nhap so phan tu cua mang (0 < n <= 100): ");
+        scanf("%d", &n);
+    } while (n <= 0 || n > 100);
+
+    for (i = 0; i < n; i++) {
+        printf("Nhap phan tu thu %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    max = arr[0];
+    for (i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    printf("Phan tu lon nhat trong mang la: %d\n", max);
+
+    return 0;
+}
